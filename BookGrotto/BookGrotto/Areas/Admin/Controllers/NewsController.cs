0f7f79e5f603285be1e96864f116d9480a96ad11
hovinh414 <hovinh414@gmail.com>
2023-02-21 15:49:27 +0700

    Update Insert News

---
+++ BookGrotto/BookGrotto/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,42 @@
+﻿using BookGrotto.Models;
+using BookGrotto.Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookGrotto.Areas.Admin.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: Admin/News
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var items = db.News.OrderByDescending(x => x.Id).ToList();
+            return View(items);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(News model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                // cái này chừng nào đủ danh mục thì sẽ thay đổi sau
+                model.CategoryId = 1;
+                model.ModifiedDate = DateTime.Now;
+                model.Alias = BookGrotto.Models.Common.Filter.FilterChar(model.Title);
+                db.News.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}

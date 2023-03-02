d2c78130610438d8628204cdfdd3fbf5d22dc548
VeiNss1403 <hoangloc1403@gmail.com>
2023-03-02 10:46:20 +0700

    Update Client By Loc

--- BookGrotto/BookGrotto/App_Start/RouteConfig.cs
+++ BookGrotto/BookGrotto/App_Start/RouteConfig.cs
@@ -19,11 +19,29 @@ namespace BookGrotto
                 namespaces: new[] { "BookGrotto.Controllers" }
             );
             routes.MapRoute(
+                name: "CheckOut",
+                url: "thanh-toan",
+                defaults: new { controller = "ShoppingCart", action = "CheckOut", alias = UrlParameter.Optional },
+                namespaces: new[] { "BookGrotto.Controllers" }
+            );
+            routes.MapRoute(
+                name: "ShoppingCart",
+                url: "gio-hang",
+                defaults: new { controller = "ShoppingCart", action = "Index", alias = UrlParameter.Optional },
+                namespaces: new[] { "BookGrotto.Controllers" }
+            );
+            routes.MapRoute(
               name: "CategoryProduct",
               url: "danh-muc-san-pham/{alias}-{id}",
               defaults: new { controller = "Products", action = "ProductCategory", id = UrlParameter.Optional },
               namespaces: new[] { "BookGrotto.Controllers" }
           );
+            routes.MapRoute(
+               name: "detailProducts",
+               url: "chi-tiet/{alias}-p{id}",
+               defaults: new { controller = "Products", action = "Detail", alias = UrlParameter.Optional },
+               namespaces: new[] { "BookGrotto.Controllers" }
+           );
             routes.MapRoute(
                name: "Products",
                url: "san-pham",

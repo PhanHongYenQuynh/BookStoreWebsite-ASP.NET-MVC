f43a7054ba4e5afcba34d411e92297d3dc28a8fb
VeiNss1403 <hoangloc1403@gmail.com>
2023-03-05 08:53:05 +0700

    update client dang nhap by loc

--- BookGrotto/BookGrotto/Controllers/HomeController.cs
+++ BookGrotto/BookGrotto/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BookGrotto.Models;
+using BookGrotto.Models.EF;
+using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,18 +11,46 @@ namespace BookGrotto.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
             return View();
         }
-
+        public ActionResult Partial_Subcrise()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Subcrise(Subscribe req)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Subscribes.Add(new Subscribe { Email=req.Email,CreatedDate= DateTime.Now});
+                db.SaveChanges();
+                return Json(new {Success=true});
+            }
+            return View("Partial_Subcrise",req);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+        public ActionResult Refesh()
+        {
+            var item = new ThongKeModel();
+            ViewBag.Visitors_online = HttpContext.Application["visitors_online"];
+            var hn = HttpContext.Application["HomNay"];
+            item.HomNay = HttpContext.Application["HomNay"].ToString();
+            item.HomQua = HttpContext.Application["HomQua"].ToString();
+            item.TuanNay = HttpContext.Application["TuanNay"].ToString();
+            item.TuanTruoc = HttpContext.Application["TuanTruoc"].ToString();
+            item.ThangNay = HttpContext.Application["ThangNay"].ToString();
+            item.ThangTruoc = HttpContext.Application["ThangTruoc"].ToString();
+            item.TatCa = HttpContext.Application["TatCa"].ToString();
+            return PartialView(item);
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
